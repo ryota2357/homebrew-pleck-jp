47962b6bad6975a46881c563858c8b7f8e76db1f
2357 <61523777+ryota2357@users.noreply.github.com>
2023-05-06 20:18:57 +0900

    init

---
+++ pleck-jp.rb
@@ -0,0 +1,14 @@
+cask "pleck-jp" do
+  version "0.1.0"
+  sha256 "5c1edcfbd175a094e341c9a2aab4ddb5b1642de7016ae2291e4e3a3a5d6ca9ea"
+
+  url "https://github.com/ryota2357/PleckJP/releases/download/v#{version}/PleckJP_v#{version}.zip"
+  name "PleckJP"
+  desc "IBM Plex Sans JP + Hack + Nerd Fonts"
+  homepage "https://github.com/ryota2357/PleckJP"
+
+  font "PleckJP_v#{version}/PleckJP-Regular.ttf"
+  font "PleckJP_v#{version}/PleckJP-Bold.ttf"
+  font "PleckJP_v#{version}/PleckJP-Italic.ttf"
+  font "PleckJP_v#{version}/PleckJP-BoldItalic.ttf"
+end
