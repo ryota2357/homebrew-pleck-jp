8ade9e481f49b944d15c87a240094480f0a21e6c
2357 <61523777+ryota2357@users.noreply.github.com>
2023-05-07 16:22:35 +0900

    bump up

--- Casks/pleck-jp.rb
+++ Casks/pleck-jp.rb
@@ -1,6 +1,6 @@
 cask "pleck-jp" do
-  version "0.1.0"
-  sha256 "f9a32c992850f48b33221cd6de8cfbcfc90c91e850aa5edfb6fbae07755f5039"
+  version "0.1.1"
+  sha256 "80ca9fe075e10ac4d1659597a0ef71a7df458e9392f04abece9e8dedb4a086cc"
 
   url "https://github.com/ryota2357/PleckJP/releases/download/v#{version}/PleckJP_v#{version}.zip"
   name "PleckJP"
